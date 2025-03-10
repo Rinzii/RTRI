4b8c49b53aa12c3c0ec6b2e7de3ba430cb8144c4
Ian Pike <Ianpike98@gmail.com>
2025-03-10 15:47:13 -0400

    Bring in initial basic structure

---
+++ include/rtri/ApiFactory.hpp
@@ -0,0 +1,20 @@
+/*
+* Copyright (c) Ian Pike
+ *
+ * RTRI is provided under the Apache-2.0 License WITH LLVM-exception.
+ * See LICENSE for more information.
+ *
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#ifndef RTRI_APIFACTORY_HPP
+#define RTRI_APIFACTORY_HPP
+
+#include <rtri/RTRIConfig.hpp>
+
+namespace RTRI_NAMESPACE
+{
+
+} // namespace RTRI_NAMESPACE
+
+#endif // RTRI_APIFACTORY_HPP
