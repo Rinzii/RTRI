403b33d4cf55b0eda7effc4474c2eceb81fbd3bc
Ian Pike <Ianpike98@gmail.com>
2025-03-13 13:09:01 -0400

    Add common enum for textures

--- include/rtri/Texture.hpp
+++ include/rtri/Texture.hpp
@@ -12,8 +12,67 @@
 
 #include "RTRIConfig.hpp"
 
+#include <cstdint>
+
 namespace rtri
 {
+	enum class TextureType : uint8_t {
+	    eTEXTURE_1D,
+	    eTEXTURE_2D,
+	    eTEXTURE_3D
+	};
+	
+	enum class Texture1DViewType : uint8_t {
+	    eSHADER_RESOURCE_1D,
+	    eSHADER_RESOURCE_1D_ARRAY,
+	    eSHADER_RESOURCE_STORAGE_1D,
+	    eSHADER_RESOURCE_STORAGE_1D_ARRAY,
+	    eCOLOR_ATTACHMENT,
+	    eDEPTH_STENCIL_ATTACHMENT,
+	    eDEPTH_READONLY_STENCIL_ATTACHMENT,
+	    eDEPTH_ATTACHMENT_STENCIL_READONLY,
+	    eDEPTH_STENCIL_READONLY
+	};
+	
+	enum class Texture2DViewType : uint8_t {
+	    eSHADER_RESOURCE_2D,
+	    eSHADER_RESOURCE_2D_ARRAY,
+	    eSHADER_RESOURCE_CUBE,
+	    eSHADER_RESOURCE_CUBE_ARRAY,
+	    eSHADER_RESOURCE_STORAGE_2D,
+	    eSHADER_RESOURCE_STORAGE_2D_ARRAY,
+	    eCOLOR_ATTACHMENT,
+	    eDEPTH_STENCIL_ATTACHMENT,
+	    eDEPTH_READONLY_STENCIL_ATTACHMENT,
+	    eDEPTH_ATTACHMENT_STENCIL_READONLY,
+	    eDEPTH_STENCIL_READONLY,
+	    eSHADING_RATE_ATTACHMENT
+	};
+	
+	enum class Texture3DViewType : uint8_t {
+	    eSHADER_RESOURCE_3D,
+	    eSHADER_RESOURCE_STORAGE_3D,
+	    eCOLOR_ATTACHMENT
+	};
+	
+	enum class BufferViewType : uint8_t {
+	    eSHADER_RESOURCE,
+	    eSHADER_RESOURCE_STORAGE,
+	    eCONSTANT
+	};
+	
+	enum class DescriptorType : uint8_t {
+	    eSAMPLER,
+	    eCONSTANT_BUFFER,
+	    eTEXTURE,
+	    eSTORAGE_TEXTURE,
+	    eBUFFER,
+	    eSTORAGE_BUFFER,
+	    eSTRUCTURED_BUFFER,
+	    eSTORAGE_STRUCTURED_BUFFER,
+	    eACCELERATION_STRUCTURE
+	};
+
 	class Texture
 	{
 	public:
